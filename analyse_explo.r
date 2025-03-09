2d0327f9120f44cd36a9d20aa8d8bdb596ac7763
Lylian <lylian.challier@universite-paris-saclay.fr>
2025-03-09 12:55:31 +0100

    Ajout d'une partie Conclusion avec analyse sur les plot des données, aussi modification de l'affichage graphique

--- analyse_explo.r
+++ analyse_explo.r
@@ -1,5 +1,7 @@
 rm(list=objects())
 graphics.off()
+options(vsc.dev.args = list(width=1200, height=800, pointsize=10, res=96))
+par(mar = c(5, 5, 5, 5))  # marges : bas, gauche, haut, droite
 # Load necessary libraries
 library(mgcv)
 library(corrplot)
@@ -50,7 +52,7 @@ plot(Data0$Date, Data0$Net_demand, type='l', xlim=range(Data0$Date, Data1$Date),
 
 # plot variables sur temps : Load, Net_demand, Solar, Wind
 col <- yarrr::piratepal("basel")
-par(mfrow=c(4,1))
+par(mfrow=c(2,2))
 plot(Data0$Date, Data0$Load, type='l', col=col[1], main="Load dans le temps")
 plot(Data0$Date, Data0$Net_demand, type='l', col=col[2], main="Net_demand dans le temps")
 plot(Data0$Date, Data0$Solar_power, type='l', col=col[3], main="Solar_power dans le temps")
@@ -61,7 +63,6 @@ par(mfrow=c(2,1))
 plot(Data0$Date, Data0$Load, type='l', col=col[1], main="Load dans le temps")
 plot(Data0$Date, Data0$Net_demand+Data0$Solar_power+Data0$Wind_power, type='l', col=col[2], main="Net_demand + Solar_power + Wind_power")
 
-
 # plot variables sur temps : net_demand, toy, Temp
 par(mfrow=c(3,1))
 plot(Data0$Date, Data0$Net_demand, type='l', col=col[1], main="Net_demand dans le temps")
@@ -69,7 +70,7 @@ plot(Data0$Date, Data0$Temp, type='l', col=col[2], main="Température dans le te
 plot(Data0$Date, Data0$toy, type='p', col=col[3], main="toy (time of year) dans le temps")
 
 # en scalant Net_load et Temp, on voit que net_demand ~ -Temp
-par(mfrow=c(4,1))
+par(mfrow=c(2,2))
 plot(Data0$Date, Data0$Net_demand, type='l', col=col[1], main="Net_demand dans le temps")
 plot(Data0$Date, Data0$Temp, type='l', col=col[2], main="Température dans le temps")
 plot(Data0$Date, scale(Data0$Net_demand), type='l', col=col[1], main="Net_demand et Temp dans le temps")
@@ -83,20 +84,20 @@ lines(Data0$Date, -scale(Data0$Temp), type='l', col=col[2])
 #########################
 
 # boxplot pour voir la corrélation entre Net_demand et les variable discrete
-par(mfrow=c(4,1))
+par(mfrow=c(2, 2))
 boxplot(Data0$Net_demand ~ Data0$BH, col=col[1], main="Net_demand si jour férié")
 boxplot(Data0$Net_demand ~ Data0$Summer_break, col=col[2], main="Net_demand si summer break")
 boxplot(Data0$Net_demand ~ Data0$WeekDays, col=col[3], main="Net_demand selon jour de la semaine")
 boxplot(Data0$Net_demand ~ Data0$DLS, col=col[4], main="Net_demand si heure d'été")
 
 # nuage pour voir la corrélation entre Net_demand et les variables continues
-par(mfrow=c(4,1))
+par(mfrow=c(2, 2))
 plot(Data0$Net_demand ~ Data0$Temp, col=col[1], main="Net_demand selon Temp")
 plot(Data0$Net_demand ~ Data0$Wind, col=col[2], main="Net_demand selon Wind")
 plot(Data0$Net_demand ~ Data0$Nebulosity, col=col[3], main="Net_demand selon Nebulosity")
 plot(Data0$Net_demand ~ Data0$toy, col=col[4], main="Net_demand selon toy (time of year)")
 
-par(mfrow=c(4,1)) # mettre month en discret ? suit pas mal le toy
+par(mfrow=c(2, 2)) # mettre month en discret ? suit pas mal le toy
 plot(Data0$Net_demand ~ Data0$Month, col=col[1], main="Net_demand selon Month")
 plot(Data0$Net_demand ~ Data0$Wind_weighted, col=col[2], main="Net_demand selon Wind_weighted")
 plot(Data0$Net_demand ~ Data0$Nebulosity_weighted, col=col[3], main="Net_demand selon Nebulosity_weighted")
@@ -106,3 +107,41 @@ plot(Data0$Net_demand ~ Data0$Temp_s95, col=col[4], main="Net_demand selon Temp_
 ###################
 ### Conclusion ###
 ###################
+
+
+### Analyse Unidim
+# On voit cycle de la Net_demand
+# Load et Net_demande très proche
+# cycle des énérgie renouvelable avec augmentation au fil de années
+# Load = Net_demand + Solar + Wind
+
+# Cycle annuel Net_demand et Temp semble inversé
+# Effectivement en scalant, Net_demand se confond presque avec -Temp
+
+
+### Analyse Bidim
+# On voit que la Net_demand est plus faible si le jour est férié
+# Pareil mais avec une différence encore plus importante pour le summer break
+# Dans la semaine, le samedi et dimanche ont plus basse Net_demand
+# => c'est du à la consommation des entreprises, industries qui sont off 
+# les jours fériés, week end et pendant le summer break
+# Pour Holiday classique la différence est très faible car moins de fermeture
+# Christmas_break ça monte car fermeture très courte pdt le break pas impactant
+
+# On voit que pdt l'heure d'été la Net_demand est plus faible 
+# => surement lien avec saison, chauffage et aussi plus direct avec l'éclairage ?
+
+# On voit corrélation entre Net_demand et Temp
+# elle est linéaire négative jusqu'à un certain point 
+# puis effet clim la fait légèrement remontée 
+# => légèrement non linéaire
+# pareil pour les versions lissées
+
+# On voit que Net_demand n'est pas corrélé avec Wind et très peu avec Nebulosity
+# pareil pour leur version weighted
+
+# Corrélation non linéaire entre Net_demand et toy (en U)
+# Clairement lié à la température, la saison
+# et les vacances => plus bas vers le summer_break
+# retrouve le même effet mais en discret pour month
+# faut-il le passer en factor ? 
